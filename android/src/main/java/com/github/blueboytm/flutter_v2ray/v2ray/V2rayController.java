c8c6547ac874e72b7dba4412ae226c3308d9a5cb
Arshia Ihammi <arshiaihammi@gmail.com>
2024-02-02 16:41:24 +0300

    fix #35

--- android/src/main/java/com/github/blueboytm/flutter_v2ray/v2ray/V2rayController.java
+++ android/src/main/java/com/github/blueboytm/flutter_v2ray/v2ray/V2rayController.java
@@ -25,12 +25,14 @@ public class V2rayController {
         Utilities.copyAssets(context);
         AppConfigs.APPLICATION_ICON = app_icon;
         AppConfigs.APPLICATION_NAME = app_name;
-        context.registerReceiver(new BroadcastReceiver() {
+
+        BroadcastReceiver receiver = new BroadcastReceiver() {
             @Override
             public void onReceive(Context arg0, Intent arg1) {
                 AppConfigs.V2RAY_STATE = (AppConfigs.V2RAY_STATES) arg1.getExtras().getSerializable("STATE");
             }
-        }, new IntentFilter("V2RAY_CONNECTION_INFO"));
+        };
+        context.registerReceiver(receiver, new IntentFilter("V2RAY_CONNECTION_INFO"), null, null);
     }
 
     public static void changeConnectionMode(final AppConfigs.V2RAY_CONNECTION_MODES connection_mode) {
